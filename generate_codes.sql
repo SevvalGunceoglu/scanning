7e3bf73a9eac2100a778b8d4b4c381c7f5f848a9
Şevval Günceoğlu <sevval.gunceoglu@hotmail.com>
2023-02-17 16:40:51 +0300

    soru 1- procedurleri

---
+++ generate_codes.sql
@@ -0,0 +1,36 @@
+CREATE PROCEDURE [dbo].[generate_codes]
+AS
+BEGIN
+  -- Koddaki karakterler
+  DECLARE @characters VARCHAR(20) = 'ACDEFGHKLMNPRTXYZ234579'
+  -- Geçici tablo için ad
+  DECLARE @tempTable NVARCHAR(128) = '#tempCodes'
+  -- Kaç adet kod üretileceði
+  DECLARE @numberOfCodes INT = 1000
+  -- Kod uzunluðu
+  DECLARE @codeLength INT = 8
+  
+  IF OBJECT_ID(N'tempdb..' + @tempTable) IS NOT NULL
+    DROP TABLE #tempCodes
+  
+  CREATE TABLE #tempCodes (Code NVARCHAR(20))
+  
+  WHILE @numberOfCodes > 0
+  BEGIN
+    DECLARE @code NVARCHAR(20) = ''
+    
+    -- Kod üretme iþlemi
+    WHILE LEN(@code) < @codeLength
+      SET @code = @code + SUBSTRING(@characters, CAST(RAND() * LEN(@characters) + 1 AS INT), 1)
+    
+    -- Kodun geçici tabloda var olup olmadýðýný kontrol etme iþlemi
+    IF NOT EXISTS (SELECT * FROM #tempCodes WHERE Code = @code)
+    BEGIN
+      INSERT INTO #tempCodes VALUES (@code)
+      SET @numberOfCodes = @numberOfCodes - 1
+    END
+  END
+  
+  SELECT Code FROM #tempCodes
+END
+
